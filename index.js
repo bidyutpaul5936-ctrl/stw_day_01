313e57da8aa16db003db609a3b81a45e9b4e85b1
Bidyut <bidyutpaul5936@gmail.com>
2026-07-26 15:15:25 +0530

    Day01

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("Hello world");
+console.log(32)
+console.log("hello world"+32)
+console.log("yo")
+let name=5
+let a=typeof name
+console.log(a)
+console.log(typeof a)
+let b=[1,2,3]
+console.log(b)
+alert("Hello")
