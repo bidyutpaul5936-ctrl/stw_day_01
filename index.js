2e3cdab6f0177acd6b1c11b5e676856f96cceff2
Bidyut <bidyutpaul5936@gmail.com>
2026-07-26 19:38:25 +0530

    obj chnge

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ console.log("Hello world");
 console.log(32)
 console.log("hello world"+32)
 console.log("yo")
-let name=5
+let name=90
 let a=typeof name
 console.log(a)
 console.log(typeof a)
-let b=[1,2,3]
+let b=[1,2,3,6,7]
 console.log(b)
 alert("Hello")
 if(2>5){
